24f0aec153d69bbd3f8ac6a8bb076ec231947b00
NGrant <niall.grant1992@gmail.com>
2017-10-01 13:22:26 +0100

    Completed miles_to_feet function

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Exercises CA
+
+## Expressions:
+- 1: Converts 13 Miles to Feet. Using 'python3 file.py miles_to_feet miles' will specifically call the miles_to_feet function and will conver 'miles' to feet instead of default value.
--- niall-grant-exercises.py
+++ niall-grant-exercises.py
@@ -3,13 +3,34 @@
 import sys
 
 def miles_to_feet(miles):
+    feet = 5280
     if not miles:
-        print('no miles')
+        miles = 13
+        
+    total = feet * int(miles)
+    return str(miles) + ' miles = ' + str(total) + ' feet'
 
+def calc_seconds():
+    hour = 7
+    min = 21
+    sec = 37
+    return
+
+def switch(key, param):
+    switch_dict = {
+        'miles_to_feet': miles_to_feet(param)
+    }
+    
+    print(switch_dict.get(key, 'Invalid Input: ' + key))
 
 def main():
     args = sys.argv[1:]
-    print(args)
+    
+    if not args:
+        miles_to_feet('')
+    else:
+        switch(args[0], args[1])
+        
 
 if __name__ == "__main__":
     main()
