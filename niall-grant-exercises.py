5176525d5fd505adb6fd88e21f5f618415f0b3ae
NGrant <niall.grant1992@gmail.com>
2017-10-01 22:51:41 +0100

    main() now prompts user with help guide if they dont specify a function. to_seconds() and miles_to_feet() now prompts user for numbers to convert

--- niall-grant-exercises.py
+++ niall-grant-exercises.py
@@ -2,35 +2,62 @@
 
 import sys
 
-def miles_to_feet(miles):
+def input_int(prompt, high):
+    int_input = 0
+    while True:
+        int_input = input(prompt)
+        if not int_input.isdigit():
+            print('Invalid input: ' + str(int_input))
+            continue
+            
+        int_input = int(int_input)
+        
+        if int_input >= high or int_input < 0:
+            print('Invalid Value: ' + str(int_input))
+            continue
+        else:
+            return int_input
+        
+
+def miles_to_feet():
+    print('MILES TO FEET:\n')
+    miles = input('No. of Miles:')
     feet = 5280
     if not miles:
+        print('No valid input detected. Miles value defaults to 13')
         miles = 13
         
     total = feet * int(miles)
     return str(miles) + ' miles = ' + str(total) + ' feet'
 
-def calc_seconds():
-    hour = 7
-    min = 21
-    sec = 37
-    return
-
-def switch(key, param):
-    switch_dict = {
-        'miles_to_feet': miles_to_feet(param)
-    }
+def to_seconds():
+    print('CONVERT TO SECONDS:')
+    hour = input_int('Hour: ', 24)
+    mins = input_int('Minutes: ', 60)
+    sec = input_int('Seconds: ', 60)
     
-    print(switch_dict.get(key, 'Invalid Input: ' + key))
+    hour_sec = hour * 3600
+    min_sec = mins * 60
+    
+    total_sec = hour_sec + min_sec + sec
+    
+    return 'Total seconds: ' + str(total_sec)
 
 def main():
     args = sys.argv[1:]
-    
     if not args:
-        miles_to_feet('')
-    else:
-        switch(args[0], args[1])
+        print('Invalid Entry.\n'
+              'Usage Guide:\n'
+              'Type: "python3 file.py [--arg]"\n\n'
+              '[--arg value] options:\n'
+              'miles_to_feet:\t\t--tofeet\n'
+              'to_seconds:\t\t--tosec\n')
         
-
+    if args[0] == '--tofeet':
+        print(miles_to_feet())
+    
+    if args[0] == '--tosec':
+        print(to_seconds())
+    
 if __name__ == "__main__":
     main()
